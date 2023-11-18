54f481ca3a386597ba82257d394f159081b6c335
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-11-18 11:27:07 +0100

    Home Page image added

--- hoteStribrava/src/pages/HomePage/index.jsx
+++ hoteStribrava/src/pages/HomePage/index.jsx
@@ -11,19 +11,29 @@ export const HomePage = () => {
           <p>Quas odio quidem, enim nihil unde quia temporibus vitae in ab.</p>
           <div class="cards-row">
             <div class="card">
-              <img class="card__image" src="img/image1.svg" />
+              <img class="card__image" src="img/pokoje/pokoj01.jpg" />
               <div class="card__title">Card 1</div>
               <div class="card__body">Sunt natus</div>
             </div>
 
             <div class="card">
-              <img class="card__image" src="img/image1.svg" />
+              <img class="card__image" src="img/pokoje/pokoj02.jpg" />
               <div class="card__title">Card 2</div>
               <div class="card__body">Laboriosam</div>
             </div>
 
             <div class="card">
-              <img class="card__image" src="img/image1.svg" />
+              <img class="card__image" src="img/pokoje/pokoj03.jpg" />
+              <div class="card__title">Card 3</div>
+              <div class="card__body">Eveniet officiis</div>
+            </div>
+            <div class="card">
+              <img class="card__image" src="img/pokoje/pokoj04.jpg" />
+              <div class="card__title">Card 3</div>
+              <div class="card__body">Eveniet officiis</div>
+            </div>
+            <div class="card">
+              <img class="card__image" src="img/pokoje/pokoj05.jpg" />
               <div class="card__title">Card 3</div>
               <div class="card__body">Eveniet officiis</div>
             </div>
